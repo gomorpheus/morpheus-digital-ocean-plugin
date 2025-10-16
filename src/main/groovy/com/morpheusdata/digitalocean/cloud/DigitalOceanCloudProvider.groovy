569cb884fd779cd07e829692edd5aa67aa5d6561
Sydney Lim <sydney@morpheusdata.com>
2025-10-16 11:39:08 -0700

    Revert "[PCCP-6750] sl - enable security group active checkbox on cloud"
    This reverts commit 751bda5a48436c2d6bde02809cf0f8ae4b2074ad.

--- src/main/groovy/com/morpheusdata/digitalocean/cloud/DigitalOceanCloudProvider.groovy
+++ src/main/groovy/com/morpheusdata/digitalocean/cloud/DigitalOceanCloudProvider.groovy
@@ -123,10 +123,6 @@ class DigitalOceanCloudProvider implements CloudProvider {
 		return true
 	}
 
-	static Boolean hasSecurityGroups() {
-		return true
-	}
-
 	@Override
 	Collection<OptionType> getOptionTypes() {
 		def options = []
